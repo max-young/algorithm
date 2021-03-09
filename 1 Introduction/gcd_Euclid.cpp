00c5585f18b50cca4c6b026a9a93755760a1d925
yangle <yangle@luojilab.com>
2021-03-09 19:45:31 +0800

    better gcd euclid

--- 1 Introduction/gcd_Euclid.cpp
+++ 1 Introduction/gcd_Euclid.cpp
@@ -1,8 +1,9 @@
+// 最大公约数的欧几里得算法
 # include <iostream>
 
 using namespace std;
 
-int gcd_euclid(int &m, int &n) {
+int gcd_euclid(int m, int n) {
   while (n != 0) {
     int r =  m % n;
     m = n;
@@ -12,7 +13,9 @@ int gcd_euclid(int &m, int &n) {
 }
 
 int main() {
-  int m = 60, n = 24;
+  int m, n;
+  cout << "Please enter two non negtive integer: ";
+  cin >> m >> n;
   int r = gcd_euclid(m, n);
   cout << "great common divisor of " << m << " and " << n << " is: " << r << endl; 
 }

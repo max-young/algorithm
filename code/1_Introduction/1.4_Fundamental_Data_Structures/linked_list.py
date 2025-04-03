733ddbbc51fe14c1e2392d60681578ac3b0ca32b
Max <alvysurvey@gmail.com>
2025-04-03 15:56:53 +0800

    linked list

--- code/1_Introduction/1.4_Fundamental_Data_Structures/linked_list.py
+++ code/1_Introduction/1.4_Fundamental_Data_Structures/linked_list.py
@@ -11,6 +11,8 @@ class Node:
 
 
 class LinkedList:
+    """Linked List class that manages a collection of nodes.
+    """
 
     def __init__(self, data=None):
         if data:
@@ -32,11 +34,39 @@ class LinkedList:
         return repr_str
 
     def insert_begin(self, data):
+        """
+        Insert a new node with the given data at the beginning of the linked list.
+        
+        Args:
+            data: The data to be stored in the new node.
+            
+        Time Complexity:
+            O(1) - Constant time operation as it only updates pointers.
+            
+        Example:
+            linked_list = LinkedList()
+            linked_list.insert_begin(10)
+        """
         node = Node(data)
         node.next = self.head
         self.head = node
 
     def insert_end(self, data):
+        '''
+        Inserts a new node with the given data at the end of the linked list.
+
+        This method creates a new node with the provided data and adds it to the end of the
+        linked list. If the list is empty, the new node becomes the head of the list.
+        Otherwise, it traverses the list to find the last node and appends the new node.
+
+        Args:
+            data: The data to be stored in the new node.
+
+        Time Complexity: O(n) where n is the number of nodes in the list,
+        as it needs to traverse the entire list to find the last node.
+
+        Space Complexity: O(1) as it only creates one new node regardless of list size.
+        '''
         node = Node(data)
         if not self.head:
             self.head = node
@@ -45,8 +75,23 @@ class LinkedList:
             while current.next:
                 current = current.next
             current.next = node
-    
+        return
+
     def insert_between(self, middle, data):
+        """
+        Inserts a new node with the given data after the first node containing the middle value.
+        
+        If a node with the middle value is found, the new node is inserted after it.
+        If no node with the middle value is found, the new node is appended to the end of the list.
+        If the list is empty, the new node becomes the head of the list.
+        
+        Args:
+            middle: The value to search for in the list
+            data: The data to be stored in the new node
+            
+        Returns:
+            None
+        """
         node = Node(data)
         if not self.head:
             self.head = node
@@ -65,20 +110,49 @@ class LinkedList:
         return
 
     def delete(self, data):
+        """
+        Delete the first occurrence of a node with the given data value from the linked list.
+
+        Args:
+            data: The value to be deleted from the linked list.
+
+        Returns:
+            None. The method modifies the linked list in-place.
+
+        Note:
+            - If the list is empty, the method returns without any action.
+            - If the head node contains the data to be deleted, the head is updated to the next node.
+            - If the data is not found in the list, the list remains unchanged.
+        """
         if not self.head:
             return
         if self.head.value == data:
             self.head = self.head.next
             return
         current = self.head
-        next = current.next
-        while next:
-            if next.value == data:
-                current.next = next.next
-            current = next
-            next = next.next
+        next_node = current.next
+        while next_node:
+            if next_node.value == data:
+                current.next = next_node.next
+            current = next_node
+            next_node = next_node.next
 
     def search(self, data):
+        """
+        Search for a node with the specified data in the linked list.
+        
+        Args:
+            data: The value to search for in the linked list.
+            
+        Returns:
+            bool: True if a node with the specified data is found, False otherwise.
+            
+        Time Complexity:
+            O(n) where n is the number of nodes in the linked list.
+        
+        Space Complexity:
+            O(1) as only a constant amount of extra space is used.
+        """
         if not self.head:
             return False
         current = self.head
@@ -86,7 +160,6 @@ class LinkedList:
             if current.value == data:
                 return True
             current = current.next
-        return False
 
 if __name__ == "__main__":
     my_list = LinkedList()
